5c0097cf352a4dd78d3d4f925cf75cfb870bb8bd
weexid <weexid@gmail.com>
2023-09-05 21:34:50 +0800

    update favicon

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -1,11 +1,18 @@
 <script setup>
     import Navbar from "@/Layouts/navbar/Navbar.vue";
     import Footer from "@/Layouts/footer/Footer.vue";
+    import { Head } from "@inertiajs/vue3";
 </script>
 
 <template>
+    <Head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png">
+        <link rel="manifest" href="/favicons/site.webmanifest">
+    </Head>
     <Navbar />
-    <div class="bg-slate-300 mt-16 z-10">
+    <div class="bg-slate-300 mt-16 relative z-10">
         <slot/>
     </div>
     <Footer/>
